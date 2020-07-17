a24e7b21cf0ccfd7c065b78226372a1b670b479d
ivan san <sii200192@gmail.com>
2020-07-17 12:25:47 +0700

    Переезд на typescript тест

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ const templateIssueBlock: HTMLElement = document.getElementById('js-issue');
 const github: Github = new Github();
 
 /* -- Навешиваем события -- */
-searchForm.addEventListener('submit', search);
+// searchForm.addEventListener('submit', search);
 
 
 /* -- Инициализируем функции -- */
@@ -25,64 +25,64 @@ searchForm.addEventListener('submit', search);
  *
  * @param {Event} event
  */
-function search(event: SubmitEvent): void {
-    event.preventDefault();
-
-    resultBlock.innerHTML = '';
-
-    const elements: object = event.target.elements;
-
-    github.listRepositoryIssues(elements.owner.value, elements.repo.value)
-        .then(onSuccess)
-        .catch(onError);
-}
-
-/**
- * Обработчик ошибки при неудачном запросе Github issue
- */
-function onError(err: object): void {
-    let p = document.createElement('p');
-    p.innerText = `#${err.status} ${err.response.message}`;
-    resultBlock.append(p);
-}
-
-/**
- * Обработчик данных при успешном запросе Github issue
- *
- * @param {Array} data
- */
-function onSuccess(data: Array<object>): void {
-    if (data.length === 0) {
-        let p = document.createElement('p');
-        p.innerText = `Issue не найдены`;
-        resultBlock.append(p);
-
-        return;
-    }
-
-    data.forEach(issueHandler);
-}
-
-/**
- * Обрабатывает issue и добавляет его на страницу
- *
- * @param issue
- */
-function issueHandler(issue: object): void {
-    let cloneTemplate = templateIssueBlock.content.cloneNode(true);
-
-    cloneTemplate.querySelector('a').href = issue.html_url;
-    cloneTemplate.querySelector('.issue-comment-number span').innerText = issue.comments;
-
-    cloneTemplate
-        .querySelector('.issue-title')
-        .innerHTML = issue.labels.reduce((acc, curr) => {
-        return  acc + ` <span style="color: white; background-color: #${curr.color}">${curr.name}</span>`
-    }, issue.title);
-
-    cloneTemplate
-        .querySelector('.issue-subtitle')
-        .innerText = `#${issue.number} opened by ${issue.user.login}`;
-
-    resultBlock.append(cloneTemplate);
-}
+// function search(event: any): void {
+//     event.preventDefault();
+//
+//     resultBlock.innerHTML = '';
+//
+//     const elements: object = event.target.elements;
+//
+//     github.listRepositoryIssues(elements.owner.value, elements.repo.value)
+//         .then(onSuccess)
+//         .catch(onError);
+// }
+//
+// /**
+//  * Обработчик ошибки при неудачном запросе Github issue
+//  */
+// function onError(err: object): void {
+//     let p = document.createElement('p');
+//     p.innerText = `#${err.status} ${err.response.message}`;
+//     resultBlock.append(p);
+// }
+//
+// /**
+//  * Обработчик данных при успешном запросе Github issue
+//  *
+//  * @param {Array} data
+//  */
+// function onSuccess(data: Array<object>): void {
+//     if (data.length === 0) {
+//         let p = document.createElement('p');
+//         p.innerText = `Issue не найдены`;
+//         resultBlock.append(p);
+//
+//         return;
+//     }
+//
+//     data.forEach(issueHandler);
+// }
+//
+// /**
+//  * Обрабатывает issue и добавляет его на страницу
+//  *
+//  * @param issue
+//  */
+// function issueHandler(issue: object): void {
+//     let cloneTemplate = templateIssueBlock.content.cloneNode(true);
+//
+//     cloneTemplate.querySelector('a').href = issue.html_url;
+//     cloneTemplate.querySelector('.issue-comment-number span').innerText = issue.comments;
+//
+//     cloneTemplate
+//         .querySelector('.issue-title')
+//         .innerHTML = issue.labels.reduce((acc, curr) => {
+//         return  acc + ` <span style="color: white; background-color: #${curr.color}">${curr.name}</span>`
+//     }, issue.title);
+//
+//     cloneTemplate
+//         .querySelector('.issue-subtitle')
+//         .innerText = `#${issue.number} opened by ${issue.user.login}`;
+//
+//     resultBlock.append(cloneTemplate);
+// }
